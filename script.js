cf0841627bb9cfc638677c0caa0069102564d57a
Glitch (hello-webpage) <none>
2020-07-23 03:55:24 +0000

    🗃💁 Checkpoint ./index.html:19397419/3369 ./style.css:19397419/275 ./script.js:19397419/173

--- script.js
+++ script.js
@@ -2,4 +2,12 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
+var questionCount= 0;
+var resultScore= 0;
+
+var q1a1= document.getElemetId("q1a1")
+
+q1a1.addEvenrListener("click,result1");
+
+function result 1 (){
+}
