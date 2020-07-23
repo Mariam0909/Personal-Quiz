61a5614042f7f5bcdc476acd15377f479120370d
Glitch (hello-webpage) <none>
2020-07-23 20:03:28 +0000

    👄🌮 Checkpoint ./script.js:19397419/8:19410240/3

--- script.js
+++ script.js
@@ -12,14 +12,17 @@ var q1a1= document.getElemetId("q1a1");
 var q1a2= document.getElemetId("q1a2")
 var q1a3= document.getElemetId("q1a3");
 var q1a4= document.getElemetId("q1a4");
+
 var q2a1= document.getElemetId("q2a1");
 var q2a2= document.getElemetId("q2a2");
 var q2a3= document.getElemetId("q2a3");
 var q2a4= document.getElemetId("q2a4");
+
 var q3a1= document.getElemetId("q3a1");
-var q3a2= document.getElemetId("q3a1");
-var q3a3= document.getElemetId("q3a1");
-var q3a4= document.getElemetId("q3a1");
+var q3a2= document.getElemetId("q3a2");
+var q3a3= document.getElemetId("q3a3");
+var q3a4= document.getElemetId("q3a4");
+
 var q4a1= document.getElemetId("q4a1");
 var q4a2= document.getElemetId("q4a2");
 var q4a3= document.getElemetId("q4a3");
