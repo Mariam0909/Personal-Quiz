9feb64cef77505705e67090b1905945eb9f8c688
Glitch (hello-webpage) <none>
2020-07-23 14:45:47 +0000

    🍼🚤 Checkpoint ./script.js:19397419/755

--- script.js
+++ script.js
@@ -3,9 +3,28 @@
 
 // prints "hi" in the browser's dev tools console
 var questionCount= 0;
-var resultScore= 0;
+var result1Score= 0;
+var result3Score=0;
+var result4Score=0;
 
-var q1a1= document.getElemetId("q1a1")
+var q1a1= document.getElemetId("q1a1");
+var q1a2= document.getElemetId("q1a2")
+var q1a3= document.getElemetId("q1a3");
+var q1a4= document.getElemetId("q1a4");
+var q2a1= document.getElemetId("q2a1");
+var q2a2= document.getElemetId("q2a2");
+var q2a3= document.getElemetId("q2a3");
+var q2a4= document.getElemetId("q2a4");
+var q3a1= document.getElemetId("q3a1");
+var q3a2= document.getElemetId("q3a1");
+var q3a3= document.getElemetId("q3a1");
+var q3a4= document.getElemetId("q3a1");
+var q4a1= document.getElemetId("q4a1");
+var q4a2= document.getElemetId("q4a2");
+var q4a3= document.getElemetId("q4a3");
+var q4a4= document.getElemetId("q4a4");
+
+
 
 q1a1.addEventListener("click,blue" function result (){
 }
