aa97f11cac311507bce7336a382862891f31465a
Glitch (hello-webpage) <none>
2020-07-23 20:16:56 +0000

    🏐🌽 Checkpoint ./script.js:19397419/259:19410240/3 ./index.html:19397419/460

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 
 // prints "hi" in the browser's dev tools console
 var questionCount= 0;
-var Blue= 0;
-var Lavender=0;
-var Black=0;
-var Green=0;
+var BlueScore = 0;
+var LavenderScore = 0;
+var BlackScore = 0;
+var GreenScore = 0;
 
 var q1a1= document.getElemetId("q1a1");
 var q1a2= document.getElemetId("q1a2")
@@ -22,11 +22,6 @@ var q3a1= document.getElemetId("q3a1");
 var q3a2= document.getElemetId("q3a2");
 var q3a3= document.getElemetId("q3a3");
 var q3a4= document.getElemetId("q3a4");
-
-var q4a1= document.getElemetId("q4a1");
-var q4a2= document.getElemetId("q4a2");
-var q4a3= document.getElemetId("q4a3");
-var q4a4= document.getElemetId("q4a4");
 
     q1a1.addEventListener("click", Blue);
     q1a2.addEventListener("click", Lavender);
